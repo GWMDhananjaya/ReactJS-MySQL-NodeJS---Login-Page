cfc39cbbc7b591302a16249ffa677f892d983d0a
DHANANJAYA RUPASINGHE <106276257+Dhananjaya0216@users.noreply.github.com>
2024-03-24 12:10:07 +0530

    Add email and password state variables to Login component

--- frontend/project/src/Login.jsx
+++ frontend/project/src/Login.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 
 function Login() {
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
   return (
     <>
         <div>
             <form action="">
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type="email" placeholder='Enter Email' />
+                    <input type="email" placeholder='Enter Email' 
+                   
+                    />
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type="password" placeholder='Enter Password' />
+                    <input type="password" placeholder='Enter Password' 
+                   
+                    />
                 </div>
                 <button>Login</button>
             </form>
