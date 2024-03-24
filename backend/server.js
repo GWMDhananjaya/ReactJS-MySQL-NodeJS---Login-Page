5dc23753acdf6dfe4ea5378db993a0d415626ded
DHANANJAYA RUPASINGHE <106276257+Dhananjaya0216@users.noreply.github.com>
2024-03-24 14:35:45 +0530

    Add MySQL database connection to server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,14 @@
  const app = express();
 
  app.use(cors());
+
+ const db = mysql.createConnection({
+        host: "localhost",
+        user:"root",
+        password:"",
+        database:"login",  
+ });
+
  app.listen(3001, () => {
      console.log('Server is running on port 3001');
  });
